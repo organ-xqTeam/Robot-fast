d63835fd3a67ea507db949bd5834f8896b7f9f3c
lihaochong <1272787639@qq.com>
2019-12-31 15:21:07 +0800

    12-31

---
+++ ruoyi-manage/src/main/java/com/ruoyi/controller/AppRobotProjectController.java
@@ -0,0 +1,12 @@
+package com.ruoyi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin
+@Controller
+@RequestMapping("/ajax/app/appRobotProject")
+public class AppRobotProjectController {
+
+}
